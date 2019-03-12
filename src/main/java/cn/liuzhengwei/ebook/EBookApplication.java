0524c9f032f5c587683cf23145f428785876cd66
liuzhengwei127 <756670011@qq.com>
2019-03-12 22:23:54 +0800

    spring boot init

---
+++ src/main/java/cn/liuzhengwei/ebook/EBookApplication.java
@@ -0,0 +1,13 @@
+package cn.liuzhengwei.ebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cn/liuzhengwei/web/HelloController.java
@@ -0,0 +1,4 @@
+package cn.liuzhengwei.web;
+
+public class HelloController {
+}
