79b5a847cbb09e94ffb1235bc4d3a9aeceeb61dc
liuzhengwei127 <756670011@qq.com>
2019-03-21 10:18:30 +0800

    build an interface for OrderService

--- src/main/java/cn/liuzhengwei/ebook/service/OrderService.java
+++ src/main/java/cn/liuzhengwei/ebook/service/OrderService.java
@@ -1,4 +1,20 @@
 package cn.liuzhengwei.ebook.service;
 
+import cn.liuzhengwei.ebook.domain.Order;
+
+import java.util.List;
+
 public interface OrderService {
+
+    // 添加订单
+    Order addOrder();
+
+    // 删除订单
+    Order deleteOrder();
+
+    // 获取所有订单
+    List<Order> getAllOrders();
+
+    // 获取指定用户订单
+    Order getOrder(String account);
 }
