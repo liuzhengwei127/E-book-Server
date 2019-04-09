060b047e9f06c82ee2bdab97cbfe1a60a05aff55
liuzhengwei127 <756670011@qq.com>
2019-04-09 23:11:53 +0800

    use session to store user login state

--- src/main/java/cn/liuzhengwei/ebook/controller/UserController.java
+++ src/main/java/cn/liuzhengwei/ebook/controller/UserController.java
@@ -7,6 +7,8 @@ import cn.liuzhengwei.ebook.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 //数据库中关于user表的Restful api
@@ -37,8 +39,10 @@ public class UserController {
     // 监听'/user/login',接受用户json参数 判断用户登录态并返回json
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
-    public LoginState login(@RequestBody User user){
+    public LoginState login(@RequestBody User user, HttpServletRequest request){
         LoginState loginState = userservice.getLoginState(user.getAccount(),user.getPassword());
+        HttpSession session = request.getSession();
+        session.setAttribute("loginState", loginState);
         return loginState;
     }
 
@@ -58,10 +62,23 @@ public class UserController {
         return userToChange;
     }
 
+    // 监听'/user/states',获取所有用户的权限
     @RequestMapping(value = "/states", method = RequestMethod.GET)
     @ResponseBody
     public List<UserState> getUserStates() {
         List<UserState> userStates = userservice.getUserStates();
         return userStates;
+    }
+
+    // 监听'/user/init',判断用户是否最近登录
+    @RequestMapping(value = "/init", method = RequestMethod.GET)
+    @ResponseBody
+    public LoginState ifUserLogin(HttpServletRequest request) {
+        LoginState loginState;
+        HttpSession session = request.getSession();
+        loginState = (LoginState) session.getAttribute("loginState");
+        if (loginState == null)
+            loginState = new LoginState();
+        return loginState;
     }
 }
