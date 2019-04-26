8f3f2d2ab792f3cc777845058a377b68042bb3c2
liuzhengwei127 <756670011@qq.com>
2019-04-26 09:40:18 +0800

    mail function start!!!

--- src/test/java/cn/liuzhengwei/ebook/EBookApplicationTests.java
+++ src/test/java/cn/liuzhengwei/ebook/EBookApplicationTests.java
@@ -1,13 +1,30 @@
 package cn.liuzhengwei.ebook;
 
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EBookApplicationTests {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Test
+    public void sendMail() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("756670011@qq.com");
+        message.setTo("756670011@qq.com");
+        message.setSubject("主题：简单邮件");
+        message.setText("测试邮件内容");
+
+        mailSender.send(message);
+    }
 
 }
